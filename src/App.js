e5616bc802bd9fa3e36840bcf9faab3172bcb7cf
sunwei <me@sunwei.xyz>
2020-06-06 11:26:58 +0800

    Modified: add template for component; Wayde

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import styled from 'styled-components';
+import Loading from './Loading';
+import Filters from './Filters';
+import RestaurantList from './RestaurantList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MainColumn = styled.div`
+  max-width: 1150px;
+  margin: 0 auto;
+`;
+
+const defaultFilters = {
+  nameFilter: '',
+  priceRangeFilter: {
+    $: false,
+    $$: false,
+    $$$: false,
+    $$$$: false,
+  },
+};
+
+const defaultHistory = createBrowserHistory();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: [],
+      loading: true,
+      error: false,
+      ...defaultFilters,
+    };
+  }
+
+  componentDidMount() {
+    const host = process.env.REACT_APP_CONTENT_HOST;
+    fetch(`${host}/resource/restaurants.json`)
+      .then((result) => result.json())
+      .then((restaurants) => {
+        this.setState({
+          restaurants: restaurants.map((restaurant) => ({
+            ...restaurant,
+            imageSrc: `${host}${restaurant.imageSrc}`,
+          })),
+          loading: false,
+        });
+      })
+      .catch(() => {
+        this.setState({ loading: false, error: true });
+      });
+  }
+
+  setNameFilter = (value) => this.setState({ nameFilter: value });
+
+  setPriceRangeFilter = (range) => (checked) => {
+    this.setState(({ priceRangeFilter }) => ({
+      priceRangeFilter: {
+        ...priceRangeFilter,
+        [range]: checked,
+      },
+    }));
+  };
+
+  resetAllFilters = () => this.setState(defaultFilters);
+
+  render() {
+    const {
+      restaurants,
+      priceRangeFilter,
+      nameFilter,
+      loading,
+      error,
+    } = this.state;
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    if (error) {
+      return (
+        <MainColumn>
+          Sorry, but the restaurant list is unavailable right now
+        </MainColumn>
+      );
+    }
+
+    return (
+      <Router history={this.props.history || defaultHistory}>
+        <MainColumn>
+          <Filters
+            name={nameFilter}
+            priceRange={priceRangeFilter}
+            setNameFilter={this.setNameFilter}
+            setPriceRangeFilter={this.setPriceRangeFilter}
+            resetAll={this.resetAllFilters}
+          />
+          <RestaurantList
+            restaurants={restaurants}
+            priceRangeFilter={priceRangeFilter}
+            nameFilter={nameFilter}
+          />
+        </MainColumn>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+window.renderBrowse = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId),
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  serviceWorker.unregister();
+};
+
+window.unmountBrowse = (containerId) => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
