578c6c0438a12a84a671e54c20ce5376ef4624bf
sunwei <sunwei@users.noreply.github.com>
2020-06-07 01:36:47 +0000

    deploy: f52d9f4c3f2edde5fa744f1a0d21b2113964384e

---
+++ precache-manifest.97eed9f4fe1b1cae1c1072f151ef2059.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33df22563d1ff0e1c073d45c9d5503a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6fafeec0bcb0c7a6d81",
+    "url": "/static/css/main.1cbdac32.css"
+  },
+  {
+    "revision": "f6fafeec0bcb0c7a6d81",
+    "url": "/static/js/main.b262e556.js"
+  },
+  {
+    "revision": "d4b9583852996254d9a5b908c50798f8",
+    "url": "/static/js/main.b262e556.js.LICENSE.txt"
+  }
+]);
